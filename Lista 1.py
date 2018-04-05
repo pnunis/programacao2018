8a538e7f0c02c639c6d76b62df7b99d82a4fcb15
Paulo Luís <paulo_nunis2@hotmail.com>
2018-04-05 15:18:51 -0700

    Update Lista 1.py

--- Lista 1.py
+++ Lista 1.py
@@ -7,3 +7,14 @@ a = 1
 while a <= 10:
      print(n*a)
      a = a + 1
+    #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo 
+usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, 
+sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado. 
+"""
+kmp = int(input("Digite a quantidade de km percorridos: "))
+dias = int(input("Digite a quantidade de dias: "))
+
+print ('Valor do aluguel: R$ %.2f' %(kmp*0.15 + dias*60) )
