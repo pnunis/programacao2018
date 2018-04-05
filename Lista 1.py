5a6323104e42f1cc3342ee932278e52414cc6d48
Paulo Luís <paulo_nunis2@hotmail.com>
2018-04-05 16:02:24 -0700

    Update Lista 1.py

--- Lista 1.py
+++ Lista 1.py
@@ -14,7 +14,7 @@ Escreva um programa que pergunte a quantidade de km percorridos por um carro alu
 usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, 
 sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado. 
 """
-kmp = int(input("Digite a quantidade de km percorridos: "))
-dias = int(input("Digite a quantidade de dias: "))
+km = int(input("Digite a qtde de km rodado: "))
+dias = int(input("Digite a qtde de dias: "))
 
-print ('Valor do aluguel: R$ %.2f' %(kmp*0.15 + dias*60) )
+print ('Valor do aluguel: R$ %.2f' %(km*0.15 + dias*60) )
