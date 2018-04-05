884c32ab9bf7dd52ce7f76c71fef6edf8e8d1918
Paulo Luís <paulo_nunis2@hotmail.com>
2018-04-05 16:32:39 -0700

    Update Lista 1.py

--- Lista 1.py
+++ Lista 1.py
@@ -18,3 +18,62 @@ km = int(input("Digite a qtde de km rodado: "))
 dias = int(input("Digite a qtde de dias: "))
 
 print ('Valor do aluguel: R$ %.2f' %(km*0.15 + dias*60) )
+#3
+"""
+Faça um Programa que leia três números e mostre o maior deles.
+"""
+
+n1 = int(input('digite primeiro número: '))
+n2 = int(input('digite segundo número: '))
+n3 = int(input('digite terceiro número: '))
+
+maior = n1
+menor = n1
+
+if maior < n2:
+	maior = n2
+
+if maior < n3: 
+	maior = n3
+
+if menor > n2:
+	menor = n2
+
+if menor > n3:
+	menor = n3
+
+print ('Maior:  %d ' %maior)
+print ('Menor:  %d ' %menor)
+
+#4
+"""
+A seqüência de Fibonacci é a seguinte: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ... Sua regra de 
+formação é simples: os dois primeiros elementos são 1; a partir de então, cada elemento é a 
+soma dos dois anteriores. Faça um algoritmo que leia um número inteiro calcule o seu número 
+de Fibonacci. F1 = 1, F2 = 1, F3 = 2, etc. 
+"""
+fibo = [1,1]
+i = 0
+num = int(input("Entre com um número: "))
+
+while num > len(fibo):
+	fibo.append(fibo[i] + fibo[i+1])
+	i+=1
+
+print ('Fibonacci(%d): %d' %(num,fibo[num-1]))
+#ou
+"""
+Dados dois números inteiros positivos, determinar o máximo divisor comum entre eles usando 
+o algoritmo de Euclides. 
+"""
+n1 = int(input('Primeiro numero: '))
+n2 = int(input('Segundo numero: '))
+
+if n1 < n2:
+	n1, n2 = n2, n1
+
+r1, mdc = n1%n2, n2
+while r1 != 0:
+	r1, mdc = mdc%r1, r1
+	
+print ('O mdc de (%d,%d) é: %d'%(n1,n2,mdc))
